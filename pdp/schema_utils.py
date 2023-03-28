f9511c0d689b8677d86022141233f19893f94434
sslivkoff <stormslivkoff@gmail.com>
2023-03-28 13:43:20 -0700

    add adjustments for multichain schemas

--- pdp/schema_utils.py
+++ pdp/schema_utils.py
@@ -22,9 +22,56 @@ def parse_dataset_name(dataset: str) -> typing.Mapping[str, str]:
 
 
 def get_datatype_schema(datatype: str) -> toolsql.DBSchema:
+    import toolsql
+
     module = _get_datatype_module(datatype)
-    schema: toolsql.DBSchema = module.schema
+    schema: toolsql.DBSchema = toolsql.normalize_shorthand_db_schema(
+        module.schema
+    )
     return schema
+
+
+def get_dataset_schema(
+    dataset: str, multichain_tables: bool = False
+) -> toolsql.DBSchema:
+    import copy
+    import toolsql
+
+    # parse dataset name
+    parsed = parse_dataset_name(dataset)
+    network = parsed['network']
+
+    # load schema
+    datatype_schema = get_datatype_schema(parsed['datatype'])
+    datatype_schema = copy.deepcopy(datatype_schema)
+    datatype_schema['name'] = dataset
+
+    # make schema names dataset-specific
+    if multichain_tables:
+        # if a multichain table, add chain_id to each table
+        for table_schema in datatype_schema['tables'].values():
+
+            # make chain_id primary only if there exist other primary keys
+            is_primary = any(
+                other_column.get('primary')
+                for other_column in table_schema['columns']
+            )
+            raw_column = {
+                'name': 'chain_id',
+                'type': 'INTEGER',
+                'index': True,
+                'primary': is_primary,
+            }
+            column = toolsql.normalize_shorthand_column_schema(raw_column)
+            table_schema['columns'].append(column)
+
+    else:
+        # if a single chain table, add network to table names
+        datatype_schema['tables'] = {
+            network + '_' + k: v for k, v in datatype_schema['tables'].items()
+        }
+
+    return datatype_schema
 
 
 def _get_datatype_module(datatype: str) -> types.ModuleType:
