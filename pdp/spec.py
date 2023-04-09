b99e14834b33adb6dfddb6d9643eefe79b5cf322
sslivkoff <stormslivkoff@gmail.com>
2023-04-08 21:15:13 -0600

    add binary <-> hex interconversion helpers

--- pdp/spec.py
+++ pdp/spec.py
@@ -5,6 +5,7 @@ import typing
 
 if typing.TYPE_CHECKING:
 
+    import polars as pl
     import toolcli
     import toolsql
 
@@ -44,6 +45,11 @@ if typing.TYPE_CHECKING:
         hash: str
         n_bytes: int
 
+    PolarsExpression = typing.Union[
+        pl.type_aliases.IntoExpr,
+        typing.Sequence[pl.type_aliases.IntoExpr],
+    ]
+
 #
 # # datasets
 #
@@ -53,6 +59,7 @@ global_version = '1.0.0'
 networks = {
     1: 'ethereum',
 }
+
 
 #
 # # urls and paths
@@ -90,4 +97,52 @@ styles: toolcli.StyleTheme = {
     'option': 'bold #e5e9f0',
     'comment': '#888888',
 }
+
+
+#
+# # formats
+#
+
+def to_binary(value: str | bytes) -> bytes:
+    if isinstance(value, bytes):
+        return value
+    elif isinstance(value, str):
+        if value.startswith('0x'):
+            return bytes.fromhex(value[2:])
+        else:
+            return bytes.fromhex(value)
+    else:
+        raise Exception('invalid format: ' + str(value))
+
+
+def to_hex(
+    value: str | bytes, *, prefix: bool = True, validate: bool = True
+) -> str:
+
+    if isinstance(value, str):
+        if value.startswith('0x'):
+            if validate:
+                bytes.fromhex(value[2:])
+
+            if prefix:
+                return value
+            else:
+                return value[2:]
+        else:
+            if validate:
+                bytes.fromhex(value)
+
+            if prefix:
+                return '0x' + value
+            else:
+                return value
+
+    elif isinstance(value, bytes):
+        if prefix:
+            return '0x' + value.hex()
+        else:
+            return value.hex()
+
+    else:
+        raise Exception('invalid value format')
 
