bc814926245b56b50ef7383eb0a75d8894d2d651
sslivkoff <stormslivkoff@gmail.com>
2023-04-08 21:13:10 -0600

    add basic config using env var and ability to create dataset globs

---
+++ pdp/config_utils.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import os
+
+from pdp import schema_utils
+
+
+def get_data_root() -> str:
+    data_root = os.environ.get('PDP_DATA_ROOT')
+    if data_root is None or data_root == '':
+        raise Exception('PDP_DATA_ROOT not set, so must specify paths manually')
+    return data_root
+
+
+def get_dataset_path_template(
+    dataset: str | None = None,
+    table: str | None = None,
+    *,
+    network: str | int | None = None,
+    datatype: str | None = None,
+) -> str:
+
+    if dataset is None:
+        if network is None or datatype is None:
+            raise Exception('must specify datatype and network to get dataset name')
+        dataset = schema_utils.get_dataset_name(
+            datatype=datatype, network=network
+        )
+
+    if table is None:
+        filename = '*.parquet'
+    else:
+        filename = table + '_*.parquet'
+
+    return os.path.join(get_data_root(), dataset, filename)
+
