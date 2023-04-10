2f5f7a250110303bdd830b3ab4451cace82d21aa
sslivkoff <stormslivkoff@gmail.com>
2023-04-09 18:00:34 -0600

    add native_transfers_queries

--- pdp/datasets/native_transfers/__init__.py
+++ pdp/datasets/native_transfers/__init__.py
@@ -1,1 +1,2 @@
+from .native_transfers_queries import *
 from .native_transfers_spec import *
---
+++ pdp/datasets/native_transfers/native_transfers_queries.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import typing
+
+from pdp import query_utils
+from pdp import spec
+
+if typing.TYPE_CHECKING:
+    import polars as pl
+
+
+def get_native_transfers(
+    # filters
+    from_address: str | bytes | None = None,
+    to_address: str | bytes | None = None,
+    from_addresses: typing.Sequence[str | bytes] | None = None,
+    to_addresses: typing.Sequence[str | bytes] | None = None,
+    start_block: int | None = None,
+    end_block: int | None = None,
+    block_number: int | None = None,
+    # outputs
+    sort: bool | spec.PolarsExpression = True,
+    descending: bool = False,
+    unique_keep: typing.Literal['last', 'first', 'any'] = 'last',
+    columns: spec.PolarsExpression | None = None,
+    output_binary: bool = True,
+    # inputs
+    source_path: str | None = None,
+    network: str | int | None = None,
+    scan_kwargs: typing.Any = None,
+    collect: bool = True,
+    streaming: bool = True,
+) -> pl.DataFrame:
+
+    # collect filters
+    block_filters = {
+        'start_block': start_block,
+        'end_block': end_block,
+        'block_number': block_number,
+    }
+    binary_filters = {
+        'from_address': from_address,
+        'to_address': to_address,
+    }
+    binary_is_in_filters = {
+        'from_addresses': from_addresses,
+        'to_addresses': to_addresses,
+    }
+    filters = query_utils._create_filters(
+        binary_filters=binary_filters,
+        block_filters=block_filters,
+        binary_is_in_filters=binary_is_in_filters,
+    )
+
+    if sort and isinstance(sort, bool):
+        sort = ['block_number', 'transfer_index']
+
+    return query_utils.query(
+        datatype='native_transfers',
+        filters=filters,
+        sort=sort,
+        descending=descending,
+        columns=columns,
+        output_binary=output_binary,
+        source_path=source_path,
+        network=network,
+        unique_keep=unique_keep,
+        scan_kwargs=scan_kwargs,
+        collect=collect,
+        streaming=streaming,
+    )
+
