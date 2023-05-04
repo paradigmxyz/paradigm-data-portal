8bc74762db8c965a4d3419403a3e7628ded21f1e
sslivkoff <stormslivkoff@gmail.com>
2023-05-04 16:45:02 -0700

    improve output format of dataset cli command

--- pdp/cli/commands/dataset_command.py
+++ pdp/cli/commands/dataset_command.py
@@ -22,7 +22,10 @@ def get_command_spec() -> toolcli.CommandSpec:
 
 def dataset_command(dataset: str) -> None:
     manifest = pdp.get_dataset_manifest(dataset)
-    toolstr.print(manifest['name'], style=pdp.styles['title'])
+    toolstr.print_text_box(
+        toolstr.add_style(manifest['name'] + ' dataset', pdp.styles['metavar']),
+        style=pdp.styles['title'],
+    )
     toolstr.print_bullet(
         key='description',
         value=manifest['description'],
@@ -44,4 +47,29 @@ def dataset_command(dataset: str) -> None:
         value=toolstr.format_nbytes(total_size, decimals=1),
         styles=pdp.styles,
     )
+
+    print()
+    for table_name, table in manifest['schema']['tables'].items():
+        if len(manifest['schema']['tables']) > 1:
+            toolstr.print(
+                table_name + ' table', style=pdp.styles['content'], indent=4
+            )
+        rows = []
+        for column in table['columns']:
+            row = [column['name'], column['type'], column['description']]
+            rows.append(row)
+        labels = ['column', 'type', 'description']
+        toolstr.print_table(
+            rows,
+            labels=labels,
+            column_styles={
+                'column': pdp.styles['metavar'],
+                'type': pdp.styles['content'],
+                'description': pdp.styles['description'],
+            },
+            # style=pdp.styles['metavar'],
+            border=pdp.styles['content'],
+            label_style=pdp.styles['metavar'],
+            indent=4,
+        )
 
